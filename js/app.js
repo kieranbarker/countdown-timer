6c156e1f7fb396f74eecb2847fa0d3e15e8510c9
Kieran Barker <kieran11397@gmail.com>
2019-12-09 09:44:09 +0000

    Get a basic countdown working and then resetting once it hits zero

--- js/app.js
+++ js/app.js
@@ -1,3 +1,48 @@
 ;(function() {
+
   "use strict";
+
+  //
+  // Variables
+  //
+
+  var interval;
+
+  var data = 10;
+
+  var app = document.querySelector("#app");
+
+
+  //
+  // Functions
+  //
+
+  function template() {
+    if (data > 0) {
+      return --data;
+    }
+  }
+
+  function render() {
+    if (data > 0) {
+      app.textContent = template();
+    } else {
+      clearInterval(interval);
+      start();
+    }
+  }
+
+  function start() {
+    data = 10;
+    app.textContent = data;
+    interval = setInterval(render, 1000);
+  }
+
+
+  //
+  // Init
+  //
+
+  start();
+
 })();
